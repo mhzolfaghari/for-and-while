188285a5b5305a6195a14d4e51a332e56458f292
mhzolfaghari <mh.zolfaghari@yahoo.com>
2021-08-30 12:22:13 +0430

    math init

---
+++ testmath.py
@@ -0,0 +1,16 @@
+from math import floor
+print(floor(2.75)) #print 2
+
+from math import ceil
+print(ceil(2.25)) #print 3
+
+print(round(2.75122344,2))
+
+from math import log10
+print(log10(100)) #print 2
+
+from math import sin
+print(sin(0))# in radin
+
+from math import* # * is all lib in math
+
