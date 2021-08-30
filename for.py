e9a82552df4560c710b5406ddf9ea267de4f61a7
mhzolfaghari <mh.zolfaghari@yahoo.com>
2021-08-30 10:24:45 +0430

    تست فارسی

---
+++ for.py
@@ -0,0 +1,19 @@
+listName = ["Ali" , "Hasan" , "Abas" , "Mohammad"]
+listNumber = [20 , 19 , 18, 17]
+
+
+
+for i in range(0,4):
+    print(listName[i],"nomrash shode = ", listNumber[i])
+    break
+
+while True:
+    print(list(range(4)))
+    
+    
+    
+
+# print(listName[0],"nomrash shode = ", listNumber[0])
+# print(listName[1],"nomrash shode = ", listNumber[1])
+# print(listName[2],"nomrash shode = ", listNumber[2])
+# print(listName[3],"nomrash shode = ", listNumber[3])
