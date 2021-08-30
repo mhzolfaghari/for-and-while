e3564b4e7b2e3de670a82da2ce31df96f5b2d25d
mhzolfaghari <mh.zolfaghari@yahoo.com>
2021-08-30 11:55:43 +0430

    import fixed

--- for.py
+++ for.py
@@ -1,10 +1,16 @@
 
-import math
+from math import sqrt
 
 # x = float(input("insert your var"))
-talaii = (1+math.sqrt(5))/2
-radical5 = math.sqrt(5)
+talaii = (1+sqrt(5))/2
+radical5 = sqrt(5)
 
 for x in range(100):
     f = (float(talaii**x)-float((1-talaii)**x))/radical5
-    print(f)
+    #  print(f)
+
+
+
+str1="hello donya"
+for i in range(len(str1)):
+    print(str1[i])
